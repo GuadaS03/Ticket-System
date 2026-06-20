35960d453dde2d7fd22734a9b4d7fc5e75b20616
GuadaS03 <savallguadalupe@gmail.com>
2026-06-19 21:56:48 -0300

    Version final limpia y configurada

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager 
+from flask_login import LoginManager
+
+from config import Config 
 
 db = SQLAlchemy()
 login = LoginManager()
